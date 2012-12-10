4a7afe316ec96ac323f5a318233ef6f973cc8ab8
Joshua Rountree <joshua@joshua-x.(none)>
2012-12-09 22:23:35 -0500

    clean up

--- config/routes.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-  , async = require('async')
-
-module.exports = function (app, auth) {
-  console.log('registering Pledge controller');
-  var pledges = require('../app/controllers/pledges')
-  app.get('/add', pledges.add)
-  app.get('/sum', pledges.sum)
-}
